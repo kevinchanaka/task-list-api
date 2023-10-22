282c43cdd04ef0f1e61af825d94c17b4e7183290
Kevin Fernando <kevinchanaka1@gmail.com>
2023-10-22 13:43:54 +1100

    Refactored application

--- api/helpers.py
+++ api/helpers.py
@@ -2,12 +2,17 @@
 
 import functools
 import math
+import logging
 from flask import request
+from marshmallow import fields
 from api.services import token_service
 from api.exceptions import NotLoggedInError, InvalidPageNumber
 from api.config import API_MAX_PAGES
-from api.schemas import PageInfoSchema
+from api.schemas import PageInfoSchema, BaseSchema
+from api.exceptions import QueryError
+from api.models import PageInfo
 
+logger = logging.getLogger()
 page_info_schema = PageInfoSchema()
 
 
@@ -30,20 +35,61 @@ def login_required(func):
         if not access_token:
             raise NotLoggedInError
         payload = token_service.verify_access_token(access_token)
-        user_id: str = payload["user_id"]
-        return func(user_id=user_id, *args, **kwargs)
+        kwargs["user_id"] = payload["user_id"]
+        return func(*args, **kwargs)
 
     return wrapper
 
 
-def generate_page_info(query_params: dict, count: int):
-    page = query_params["page"]
-    limit = query_params["limit"]
-    max_page = math.ceil(count / limit)
+def get_page_info(page_info: PageInfo, count: int):
+    if count == 0:
+        max_page = 1
+    else:
+        max_page = math.ceil(count / page_info.limit)
 
-    if page > max_page or page > API_MAX_PAGES:
+    if page_info.page > max_page or page_info.page > API_MAX_PAGES:
         raise InvalidPageNumber
 
-    return page_info_schema.dump(
-        {"limit": limit, "page": page, "count": count, "max_page": max_page}
-    )
+    return {
+        "count": count,
+        "max_page": max_page,
+        "limit": page_info.limit,
+        "page": page_info.page,
+    }
+
+
+def parse_request_body(schema: BaseSchema):
+    def decorator_parse_request_body(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            body = request.get_json()
+            parsed_data = schema.load_validate(**body)
+            return func(data=parsed_data, *args, **kwargs)
+
+        return wrapper
+
+    return decorator_parse_request_body
+
+
+def parse_request_params(schema: BaseSchema):
+    def decorator_parse_request_params(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            params = {}
+            for k in request.args.keys():
+                schema_field = schema.fields.get(k)
+                if schema_field is None:
+                    raise QueryError
+                value = request.args.getlist(k)
+                if isinstance(schema_field, fields.List):
+                    params[k] = value
+                elif len(value) > 1:
+                    raise QueryError
+                else:
+                    params[k] = value[0]
+            processed_params = schema.load_validate(**params)
+            return func(params=processed_params, *args, **kwargs)
+
+        return wrapper
+
+    return decorator_parse_request_params
--- api/schemas.py
+++ api/schemas.py
@@ -1,13 +1,25 @@
+import logging
 from typing import Callable
 from marshmallow import fields, validate, Schema, post_dump, post_load, exceptions
 from api.config import NAME_LENGTH, DEFAULT_LENGTH
-from api.exceptions import ValidationError, QueryError
+from api.exceptions import ValidationError
 from api import models
+from uuid import UUID
+
+logger = logging.getLogger()
 
 
 def camelcase(s):
     parts = iter(s.split("_"))
     return next(parts) + "".join(i.title() for i in parts)
+
+
+def is_uuid(value):
+    try:
+        UUID(value)
+    except ValueError:
+        logger.info(f"{value} is not a valid UUID")
+        raise ValidationError
 
 
 class BaseSchema(Schema):
@@ -28,28 +40,18 @@ class BaseSchema(Schema):
         try:
             data = self.load(dict_data)
         except exceptions.ValidationError as e:
-            print(e)
-            raise ValidationError
-        return data
-
-
-class BaseDictSchema(Schema):
-    def load_validate(self, **dict_data):
-        try:
-            data = self.load(dict_data)
-        except exceptions.ValidationError as e:
-            print(e)
+            logger.info(e)
             raise ValidationError
         return data
 
 
 class TaskSchema(BaseSchema):
-    id = fields.UUID(required=True)
+    id = fields.Str(required=True, validate=is_uuid)
     name = fields.Str(required=True, validate=validate.Length(min=1, max=NAME_LENGTH))
     description = fields.Str(
         required=True, validate=validate.Length(min=1, max=DEFAULT_LENGTH)
     )
-    user_id = fields.UUID(required=True)
+    user_id = fields.Str(required=True)
     completed = fields.Boolean(required=True)
     created_at = fields.DateTime()
     updated_at = fields.DateTime()
@@ -58,8 +60,11 @@ class TaskSchema(BaseSchema):
     _model = models.Task
 
 
-class TaskLabelSchema(BaseDictSchema):
-    labels = fields.List(fields.UUID)
+class TaskLabelSchema(BaseSchema):
+    task_id = fields.Str(required=True, validate=is_uuid)
+    label_ids = fields.List(fields.Str(validate=is_uuid))
+
+    _model = models.TaskLabelAssociation
 
 
 class TokenSchema(Schema):
@@ -70,7 +75,7 @@ class TokenSchema(Schema):
 
 
 class UserSchema(BaseSchema):
-    id = fields.UUID(required=True)
+    id = fields.Str(required=True, validate=is_uuid)
     name = fields.Str(required=True, validate=validate.Length(min=1, max=NAME_LENGTH))
     email = fields.Email(
         required=True, validate=validate.Length(min=1, max=NAME_LENGTH)
@@ -84,38 +89,24 @@ class UserSchema(BaseSchema):
 
 
 class LabelSchema(BaseSchema):
-    id = fields.UUID(required=True)
+    id = fields.Str(required=True, validate=is_uuid)
     name = fields.Str(required=True, validate=validate.Length(min=1, max=NAME_LENGTH))
     colour = fields.Str(required=True, validate=validate.Length(min=7, max=7))
-    user_id = fields.UUID(required=True)
+    user_id = fields.Str(required=True)
     created_at = fields.DateTime()
     updated_at = fields.DateTime()
 
     _model = models.Label
 
 
-class QueryParamsSchema(Schema):
+class PageInfoSchema(BaseSchema):
     limit = fields.Int(load_default=10)
     page = fields.Int(load_default=1)
 
-    def load_validate(self, **dict_data):
-        try:
-            data = self.load(dict_data)
-        except exceptions.ValidationError as e:
-            print(e)
-            raise QueryError
-        return data
+    _model: Callable = models.PageInfo
 
 
-class TaskParamsSchema(QueryParamsSchema):
-    labels = fields.List(fields.UUID, load_default=[])
+class TaskQuerySchema(PageInfoSchema):
+    label = fields.List(fields.Str(validate=is_uuid), load_default=[])
 
-
-class PageInfoSchema(Schema):
-    def on_bind_field(self, field_name, field_obj):
-        field_obj.data_key = camelcase(field_obj.data_key or field_name)
-
-    limit = fields.Int()
-    page = fields.Int()
-    count = fields.Int()
-    max_page = fields.Int()
+    _model = models.TaskQuery
